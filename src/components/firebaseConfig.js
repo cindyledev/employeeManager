04afd41a599d2894171439a287e9c8902aed1f02
Cindy Le <cle15@myseneca.ca>
2019-10-06 20:44:08 -0400

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyAxexsjjxpshGKwkjT1U5aLY34zlhoEFWU",
+  authDomain: "vuefs-prod-8286e.firebaseapp.com",
+  databaseURL: "https://vuefs-prod-8286e.firebaseio.com",
+  projectId: "vuefs-prod-8286e",
+  storageBucket: "vuefs-prod-8286e.appspot.com",
+  messagingSenderId: "319532458129",
+  appId: "1:319532458129:web:99157ea8cde9f274f21f30"
+};
